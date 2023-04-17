1243325e25471347d3b266e28d797c60aad73c5b
Anuj Modi <anujmodi1@yahoo.com>
2023-04-17 16:13:33 +0530

    value prep unpack rootfs and sh changes

--- tasks/aws_deploy/lab_vars.py
+++ tasks/aws_deploy/lab_vars.py
@@ -1,5 +1,5 @@
 name='ap-south-1a'
-region='uap-south-1'
+region='ap-south-1'
 az='ap-south-1a'
 ubuntu_ami_id='ami-05117a5348bb8a291'
 vault_addr='http://dev-vault.ciscolivedemo2022.com:8200'
--- tasks/aws_deploy/vault_prep.sh
+++ tasks/aws_deploy/vault_prep.sh
@@ -7,6 +7,11 @@ export VAULT_TOKEN=$SSH_TOKEN
 vault login --no-print $VAULT_TOKEN
 #Writing the AZ and Region to the vault
 #This script imports the lab_vars.py file and writes the vars to the vault under the az name mount
+vault kv list concourse/main/
+#Writing the az and region to the vault
+#this scripts imports the lab.vars.py
+echo "successfully authentication"
+#echo $VAULT_TOKEN
 cd git-resource/tasks/aws_deploy
 #python3 vault_prep.py
 
