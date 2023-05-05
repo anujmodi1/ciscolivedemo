62f632da9d85f19d412a0372c39a3216eb12ff5f
Anuj Modi <anujmodi1@yahoo.com>
2023-05-05 12:29:48 +0530

    us-west-1a changes

--- Section05_Building_the_Pipeline/input/te_install_vault/install_te.sh
+++ Section05_Building_the_Pipeline/input/te_install_vault/install_te.sh
@@ -4,7 +4,7 @@ export AWS_PAGER=""
 export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
 vault login --no-print $VAULT_TOKEN
-export te-group=$(vault kv get -field=token concourse/cisco-fso-labs/$name/te-group)
+export te-group=$(vault kv get -field=token concourse/main/$name/te-group)
 echo $te-group >> ~/.bashrc
 curl -Os https://downloads.thousandeyes.com/agent/install_thousandeyes.sh
 chmod a+x install_thousandeyes.sh
