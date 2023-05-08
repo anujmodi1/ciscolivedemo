367a12eae39302cb8dddcead3e59c56132dddf2d
Anuj Modi <anujmodi1@yahoo.com>
2023-05-08 17:59:09 +0530

    te group printing

--- Section05_Building_the_Pipeline/input/te_install_vault/install_te.sh
+++ Section05_Building_the_Pipeline/input/te_install_vault/install_te.sh
@@ -5,6 +5,7 @@ export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
 vault login --no-print $VAULT_TOKEN
 export te-group=$(vault kv get -field=token concourse/main/$name/te-group)
+echo $te-group
 echo $te-group >> ~/.bashrc
 curl -Os https://downloads.thousandeyes.com/agent/install_thousandeyes.sh
 chmod a+x install_thousandeyes.sh
