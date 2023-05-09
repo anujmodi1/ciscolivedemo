29f5e0fe2852e8a7cf03bdff1d5bec2983a49550
Anuj Modi <anujmodi1@yahoo.com>
2023-05-09 19:00:32 +0530

    delete te agent commands

--- Section05_Building_the_Pipeline/input/te_install_vault/delete_te_agents.sh
+++ Section05_Building_the_Pipeline/input/te_install_vault/delete_te_agents.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 cd git-resource/Section05_Building_the_Pipeline/input/te_install_vault
 export AWS_PAGER=""
-pip3 install paramiko
-#apt -y install ncurses-term
-#Call the vault and set the SSH key to env var
+export NAME=us-west-1a
+#apt -y update && apt -y upgrade
+python3 -m pip install paramiko -U
 export VAULT_ADDR=$VAULT_ADDR
 export SSH_TOKEN=$SSH_TOKEN
-export TE_GROUP=$TE_GROUP
 vault login --no-print $SSH_TOKEN
-vault kv get --field=ssh-key concourse/main/$NAME >> sshkey.pem
-SSHKEY='sshkey.pem'
+export te_group=$(vault kv get --field=token concourse/main/te-group)
+echo $te_group
+#cat te-group.txt
+vault kv get --field=ssh-key concourse/main/$NAME/ssh-key >> sshkey.pem
 chmod 400 sshkey.pem
+SSHKEY='sshkey.pem'
 mkdir ~/.ssh
 touch ~/.ssh/known_hosts
 echo "${SSHKEY}" | ssh-add -
-for server in $(cat hostfile)
-do
-  ssh-keyscan -H "$server" >> ~/.ssh/known_hosts
-done
+export server=$(vault kv get --field=ubuntu_ip concourse/main/$NAME/ubuntu_ip)
+echo "The external ip for the ubuntu instance on the router subnet is....."
+echo $server
+ssh-keyscan -H "$server" >> ~/.ssh/known_hosts
+scp -i sshkey.pem install_te.sh ubuntu@"$server":~/
+#ssh -i sshkey.pem ubuntu@"$server"
+echo "Calling the python script to delete TE Agent"
 python3 delete_te_agents.py
 
 
