1536f4341e0a8422455f78ea17c6d4b82a22cc09
Anuj Modi <anujmodi1@yahoo.com>
2023-05-12 10:56:16 +0530

    mysql-01 file address update 3

--- Section06_AppD_deployment/input/setup_supercar_trader_app_v2/input/supercar_trader.sh
+++ Section06_AppD_deployment/input/setup_supercar_trader_app_v2/input/supercar_trader.sh
@@ -31,10 +31,11 @@ echo $MYSQL_ROOT_PASSWORD
 apt -y update
 apt -y install mysql-client
 git clone https://github.com/sherifadel90/AppDynamics-SupercarsJavaApp.git
+cd AppDynamics-SupercarsJavaApp/supercar/src/main/resources/db
 echo "waiting for mysql loadBalancer to be provisioned in AWS....."
 sleep 3m
 MYSQL_LB=$(kubectl get svc --namespace supercar mysql -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
-cd Section06_AppD_deployment/input/setup_supercar_trader_app_v2/input
+cd AppDynamics-SupercarsJavaApp/supercar/src/main/resources/db
 mysql -h $MYSQL_LB -uroot -p"$MYSQL_ROOT_PASSWORD" < mysql-01.sql --force
 
 
