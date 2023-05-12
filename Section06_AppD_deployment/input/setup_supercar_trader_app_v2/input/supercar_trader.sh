7eaf475302b002d6326ac1cf05866aef03f5f4de
Anuj Modi <anujmodi1@yahoo.com>
2023-05-12 20:39:15 +0530

    mysql-01 file address update 16

--- Section06_AppD_deployment/input/setup_supercar_trader_app_v2/input/supercar_trader.sh
+++ Section06_AppD_deployment/input/setup_supercar_trader_app_v2/input/supercar_trader.sh
@@ -15,7 +15,7 @@ vault --version
 vault status
 mkdir ~/.kube
 vault kv get -field config concourse/main/lab-kube-config > ~/.kube/config
-vault kv get -field data concourse/main/appd-controller-info | base64 -d > ./git-resource/Section06_AppD_deployment/input/setup_supercar_trader_app_v2/input/controller-info.xml
+vault kv get -field data concourse/main/appd-controller-info | base64 -d > controller-info.xml
 kubectl get nodes
 kubectl create ns supercar
 #vault kv get -field data concourse/main/supercar-values | base64 -d > values.yaml
@@ -28,7 +28,7 @@ kubectl -n supercar apply -f supercar-trader.yml
 kubectl -n supercar apply -f tomcat_lb.yml
 kubectl -n supercar get svc
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install -n supercar mysql bitnami/mysql -f ./git-resource/Section06_AppD_deployment/input/setup_supercar_trader_app_v2/input/mysql-values.yaml
+helm install -n supercar mysql bitnami/mysql -f mysql-values.yaml
 kubectl get nodes
 #kubectl get pods -w --namespace supercar
 MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace supercar mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d)
