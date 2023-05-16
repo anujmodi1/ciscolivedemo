dbdf98eccf078021885f0011e8fe7a5b22cf1a16
Anuj Modi <anujmodi1@yahoo.com>
2023-05-16 10:24:08 +0530

    changed in the controller-info.xml

--- Section06_AppD_deployment/input/Appd_agent_deployment/supercar_agent.sh
+++ Section06_AppD_deployment/input/Appd_agent_deployment/supercar_agent.sh
@@ -16,12 +16,12 @@ kubectl exec -it $tomcat_sc -n supercar /bin/sh
 yum update
 yum install unzip
 
-curl -L -O -H "Authorization: Bearer x.xMmNBRGVqQ0RQQXBsUUVKQ0tuR0EiLCJzdWIiOiJjaXNjb2xpdmVkZW1vIiwiaWRUeXBlIjoiQVBJX0NMSUVOVCIsImlkIjoiZWVjN2M3ZDctNTJmZS00NzM4LWIyYWQtMzIyMmUzM2I4ZDlmIiwiYWNjdElkIjoiNDEzMTk1MmItNmNhOC00NWUyLWFiYmQtNTcyMGYxZmQ2YjhjIiwidG50SWQiOiI0MTMxOTUyYi02Y2E4LTQ1ZTItYWJiZC01NzIwZjFmZDZiOGMiLCJhY2N0TmFtZSI6ImNpc2NvLWFwaXBhcnRuZXJ0cmFpbmluZ2xhYiIsInRlbmFudE5hbWUiOiIiLCJmbW1UbnRJZCI6bnVsbCwiYWNjdFBlcm0iOltdLCJyb2xlSWRzIjpbXSwiaWF0IjoxNjg0MTM0NDU5LCJuYmYiOjE2ODQxMzQzMzksImV4cCI6MTY4NDIyMDg1OSwidG9rZW5UeXBlIjoiQUNDRVNTIn0.30MgVuNZFdethGyr-znd3npIvuohv3QQC31Uy8jFlys;" "https://download.appdynamics.com/download/prox/download-file/sun-jvm/22.4.0.33722/AppServerAgent-22.4.0.33722"
+curl -L -O -H "Authorization: Bearer xxxx;" "https://download.appdynamics.com/download/prox/download-file/sun-jvm/22.4.0.33722/AppServerAgent-22.4.0.33722"
 
 https://download.appdynamics.com/download/prox/download-file/java-jdk8/23.4.0.34758/AppServerAgent-1.8-23.4.0.34758.zip
 Click
 
-curl -L -O -H "Authorization: Bearer x.x.30MgVuNZFxxdethGyr-znd3npIvuohv3QQC31Uy8jFlys;" "https://download.appdynamics.com/download/prox/download-file/sun-jvm/java-jdk8/22.4.0.33722/AppServerAgent-1.8-22.4.0.33722.zip"
+curl -L -O -H "Authorization: Bearer xxx;" "https://download.appdynamics.com/download/prox/download-file/sun-jvm/java-jdk8/22.4.0.33722/AppServerAgent-1.8-22.4.0.33722.zip"
 
 java-jdk8/22.4.0.33722/AppServerAgent-1.8-22.4.0.33722.zip
 
