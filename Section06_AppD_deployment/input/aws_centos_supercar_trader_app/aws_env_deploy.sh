279b0bba33256624e11eac66413a926bef2c187f
Anuj Modi <anujmodi1@yahoo.com>
2023-05-17 13:51:30 +0530

    supercar app with ami 4

--- Section06_AppD_deployment/input/aws_centos_supercar_trader_app/aws_env_deploy.sh
+++ Section06_AppD_deployment/input/aws_centos_supercar_trader_app/aws_env_deploy.sh
@@ -4,4 +4,4 @@ export VAULT_ADDR=$VAULT_ADDR
 export VAULT_TOKEN=$SSH_TOKEN
 vault login --no-print $VAULT_TOKEN
 export NAME=$NAME
-python3 git-resource/Section05_Building_the_Pipeline/input/aws_vm_deploy/aws-deploy-env-train.py
+python3 git-resource/Section06_AppD_deployment/input/aws_centos_supercar_trader_app/aws-deploy-env-train.py
