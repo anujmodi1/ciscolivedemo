4f9b800c6dc6b9214e748e99d5bf08951abd0542
Anuj Modi <anujmodi1@yahoo.com>
2023-05-18 17:58:10 +0530

    Appd Centos instance manual install + appd task successfully captured

--- Section06_AppD_deployment/input/lab_vars/us-west-2a/lab_vars.py
+++ Section06_AppD_deployment/input/lab_vars/us-west-2a/lab_vars.py
@@ -1,4 +1,4 @@
 name='us-west-2a'
 region='us-west-2'
 az='us-west-2a'
-centos_ami_id='ami-02d7e7d9fc7e34976'
+centos_ami_id='ami-0968c0e727af0ec17'
---
+++ Section07_te_api_tasks/input/task_1/te_api.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+/usr/local/bin/python3 -m pip install --upgrade pip
+export AWS_PAGER=""
+export VAULT_ADDR=$VAULT_ADDR
+export VAULT_TOKEN=$SSH_TOKEN
+vault login --no-print $SSH_TOKEN
+TE_OATHTOKEN=$(vault kv get --field=token concourse/cisco-fso-labs/te-api)
+export TE_OATHTOKEN=$TE_OATHTOKEN
+python3 te_api_agent_server_tests.py
+
+
