120e18fb25ee01f68380fbe2e5ef63056534c368
EC2 Default User <ec2-user@ip-172-31-84-132.ec2.internal>
2023-08-19 14:53:07 +0000

    pipeline building testing

--- Section04-Testing_basic_workflow/input/basic_workflow.sh
+++ Section04-Testing_basic_workflow/input/basic_workflow.sh
@@ -7,7 +7,10 @@ export VAULT_TOKEN=$SSH_TOKEN
 vault login --no-print $VAULT_TOKEN
 #Writing the AZ and Region to the vault
 #This script imports the lab_vars.py file and writes the vars to the vault under the az name mount
-vault kv list concourse/main/
+vault kv get concourse/test
+vault kv get concourse/ssh-token
+vault kv get concourse/main/ssh-token
+#vault kv list concourse/main/
 #Writing the az and region to the vault
 #this scripts imports the lab.vars.py
 echo "successfully authentication"
